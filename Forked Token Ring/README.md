54a01732b3e2811b2bd332adb0a081eca5c41c3b
Nicholas (Cole) Burgi <nburgi@uoguelph.ca>
2025-04-05 14:19:32 -0400

    Add files via upload

---
+++ Forked Token Ring/README.md
@@ -0,0 +1,34 @@
+Cole Burgi 1174454
+
+# Semaphore Methodology
+The program uses 22 semaphores
+- 1 `EMPTY` for each node (7) to state if that node's shared byte is empty
+- 1 `FILLED` for each node (7) to state if that node's shared byte is filled
+- 1 `TO_SEND` for each node (7) to state if that node is open to receiving a new packet
+- 1 `CRIT` for critical sections
+
+The empty and filled semaphores create a producer-consumer pair at each node's shared byte, and since the program expects to consume every byte produced and only the producer (previous node) and consumer (this node) expect to access the shared byte there is no need to create a critical section when accessing the shared byte.
+
+The critical semaphore is used to create a critical section whenever a node (or fork) attempts to access a piece of shared memory that the parent process is also able to access, which happens in `token_node` to get a packet's length and in `send_pkt` to get all of the packet's data.
+
+# Packet sending Methodology
+The node opens a critical section to read the length of the packet in its shared memory, as if it is greater than 0 the node knows it has data to send.
+
+The node uses a For loop to send the packet, calling `send_pkt` then `rcv_byte` each iteration, then calls `send_byte` to send the 'available' `TOKEN`. This for loop runs for the length of the data + 4 (for the packet's headers). 
+
+After sending, the node opens a critical section to set the length in its shared memory to 0, then signals its `TO_SEND` to tell the parent process it is open to receiving a new packet.
+
+If the node doesn't have data to send, it simply forwards the `TOKEN` to the next node.
+
+If the `TOKEN` is the 'available' `TOKEN` and the node has been set to terminate (which it checks by opening a critical section and accessing its `terminate` shared variable), the node ends its while loop and exits.
+
+## snd_pkt
+This function needs to open a critical section every time it gets a byte from the node's shared packet memory to ensure it doesn't come into conflict with the parent, even though there is a very small window after the parent fills `len` where the node may try to access memory it is filling.
+
+## send_byte
+This function needs to access the shared byte of the next node to the one calling it, which it determines by the below calculation. It waits on the `EMPTY` semaphore of the next node before accessing its shared byte, then singals its `FILLED` semaphore.
+
+`next = (num + 1) % N_NODES`
+
+## rvy_byte
+This function waits on the `FILLED` semaphore of the node calling it before accessing its shared byte, then signals its `EMPTY` semaphore.
---
+++ Forked Token Ring/tokenRing_simulate.c
@@ -0,0 +1,227 @@
+//Cole Burgi 1174454
+
+/*
+ * The program simulates a Token Ring LAN by forking off a process
+ * for each LAN node, that communicate via shared memory, instead
+ * of network cables. To keep the implementation simple, it jiggles
+ * out bytes instead of bits.
+ *
+ * It keeps a count of packets sent and received for each node.
+ */
+#include <stdio.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include "tokenRing.h"
+
+#define TOKEN '1'
+#define NO_TOKEN '0'
+
+/*
+ * This function is the body of a child process emulating a node.
+ */
+void
+token_node(control, num)
+	struct TokenRingData *control;
+	int num;
+{
+	int rcv_state = TOKEN_FLAG, not_done = 1, sending = 0, len;
+	unsigned char byte;
+	/*
+	 * If this is node #0, start the ball rolling by creating the
+	 * token.
+	 */
+	if(num == 0){
+        //put token in its shared memory
+        WAIT_SEM(control, EMPTY(0));
+        control->shared_ptr->node[num].data_xfer = TOKEN; 
+        SIGNAL_SEM(control, FILLED(0));
+    }
+
+	/*
+	 * Loop around processing data, until done.
+	 */
+	while (not_done) {
+
+        byte = rcv_byte(control, num);
+
+		/*
+		 * Handle the byte, based upon current state.
+		 */
+		switch (rcv_state) {
+		case TOKEN_FLAG:
+            WAIT_SEM(control, CRIT);
+            //check if this node has a data_pkt to send
+            char send_len = control->shared_ptr->node[num].to_send.length;
+
+            SIGNAL_SEM(control, CRIT);
+			if(byte == TOKEN && send_len > 0){
+                control->shared_ptr->node[num].sent ++;
+                control->snd_state = TOKEN_FLAG;
+                
+                for(int i=0;i<send_len+4;i++){
+                    send_pkt(control, num);
+                    rcv_byte(control, num);
+                }
+                send_byte(control, num, TOKEN);
+                
+                WAIT_SEM(control, CRIT);
+                //say that this node no longer has a data_pkt to send
+                control->shared_ptr->node[num].to_send.length = 0;
+                SIGNAL_SEM(control, CRIT);
+                //ask for a new data_pkt for this node
+                SIGNAL_SEM(control, TO_SEND(num));
+            }else{
+                send_byte(control, num, byte);
+                if(byte != TOKEN){
+                    rcv_state ++;
+                }else{
+                    WAIT_SEM(control, CRIT);
+                    if(control->shared_ptr->node[num].terminate == 1){
+                        not_done = 0;
+                    }
+                    SIGNAL_SEM(control, CRIT);
+                }
+            }
+            
+			break;
+
+		case TO:
+			if(byte == (char)num){
+                control->shared_ptr->node[num].received ++;
+            }
+            
+            send_byte(control, num, byte);
+            rcv_state ++;
+			break;
+
+		case FROM:
+
+            send_byte(control, num, byte);
+            rcv_state ++;
+			break;
+
+		case LEN:
+			len = byte;
+
+            send_byte(control, num, byte);
+            rcv_state ++;
+			break;
+            
+        case DATA:
+            send_byte(control, num, byte);
+            sending ++;
+
+            if(sending == len){
+                sending = 0;
+                rcv_state = TOKEN_FLAG;
+            }
+			break;
+		};
+	}
+    exit(1);
+}
+
+/*
+ * This function sends a data packet followed by the token, one byte each
+ * time it is called.
+ */
+void
+send_pkt(control, num)
+	struct TokenRingData *control;
+	int num;
+{
+	static int sndpos, sndlen;
+    char byte;
+
+	switch (control->snd_state) {
+	case TOKEN_FLAG:
+        WAIT_SEM(control, CRIT);
+        byte = control->shared_ptr->node[num].to_send.token_flag;
+        SIGNAL_SEM(control, CRIT);
+
+        control->snd_state ++;
+		break;
+        
+    case TO:
+		WAIT_SEM(control, CRIT);
+        byte = control->shared_ptr->node[num].to_send.to;
+        SIGNAL_SEM(control, CRIT);
+
+        control->snd_state ++;
+		break;
+        
+    case FROM:
+        WAIT_SEM(control, CRIT);
+        byte = control->shared_ptr->node[num].to_send.from;
+        SIGNAL_SEM(control, CRIT);
+
+        control->snd_state ++;
+		break;
+        
+    case LEN:
+        WAIT_SEM(control, CRIT);
+        byte = control->shared_ptr->node[num].to_send.length;
+        SIGNAL_SEM(control, CRIT);
+        sndlen = byte;
+        sndpos = 0;
+
+        control->snd_state ++;
+		break;
+        
+    case DATA:
+        WAIT_SEM(control, CRIT);
+        byte = control->shared_ptr->node[num].to_send.data[sndpos];
+        SIGNAL_SEM(control, CRIT);
+        
+        sndpos ++;
+        if(sndpos == sndlen){
+            control->snd_state ++;
+        }
+		break;
+        
+    case DONE:
+		break;
+	};
+
+    send_byte(control, num, byte);
+}
+
+/*
+ * Send a byte to the next node on the ring.
+ */
+void
+send_byte(control, num, byte)
+	struct TokenRingData *control;
+	int num;
+	unsigned byte;
+{
+    int next = (num+1) % N_NODES;
+
+	WAIT_SEM(control, EMPTY(next));
+    control->shared_ptr->node[next].data_xfer = byte;
+    SIGNAL_SEM(control, FILLED(next));
+}
+
+/*
+ * Receive a byte for this node.
+ */
+unsigned char
+rcv_byte(control, num)
+	struct TokenRingData *control;
+	int num;
+{
+	unsigned char byte;
+
+	WAIT_SEM(control, FILLED(num));
+    byte = control->shared_ptr->node[num].data_xfer;
+    SIGNAL_SEM(control, EMPTY(num));
+    return byte;
+}
+
